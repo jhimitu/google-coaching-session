0f0ac5b0094c91ece0b2d857bdbfb58d2d5aa9b5
Jhia Turner <jhimitu@gmail.com>
2019-01-15 13:14:07 -0700

    first commit

---
+++ Set.js
@@ -0,0 +1,3 @@
+// [2] Implement a set-like data structure that supports Insert, Remove, and GetRandomElement efficiently. 
+// Example: If you insert the elements 1, 3, 6, 8 and remove 6, the structure should contain [1, 3, 8]. 
+// Now, GetRandom should return one of 1, 3 or 8 with equal probability. 
---
+++ sumTarget.js
@@ -0,0 +1,4 @@
+// [1] Given an array of integers, determine whether or not there exist two elements 
+// in the array (at different positions) whose sum is equal to some target value. 
+// Examples: [5, 4, 2, 4], 8 --> true [5, 1, 2, 4], 8 --> false
+
